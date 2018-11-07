bb5beff674f6c87987a43eed996adfba88dc6550
LemurPwned <jakub.mojsiejuk@gmail.com>
2018-11-07 01:00:02 +0100

    adding globals

---
+++ backup_app/lib/globals.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+final TextStyle newStyle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 13.0,
+    letterSpacing: -0.12,
+    fontWeight: FontWeight.normal);
+
+final TextStyle secondaryStyle = TextStyle(
+    fontFamily: "Roboto",
+    fontSize: 12.0,
+    letterSpacing: -0.12,
+    fontWeight: FontWeight.normal);
+
+Widget getNothingScreen(String text) {
+  return Center(
+      child: Container(
+          child: Text(
+    text,
+    style: newStyle,
+  )));
+}
